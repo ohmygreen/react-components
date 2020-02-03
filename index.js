851d894f3b3bae0b249110f1dd89b6d168ed9bd2
Nemanja Lazarevic <nemanja.lazarevic.la@gmail.com>
2020-02-03 18:48:26 +0000

    change lib to dist directory

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./lib/prod");
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./dist/prod');
 } else {
-  module.exports = require("./lib/dev");
+  module.exports = require('./dist/dev');
 }
