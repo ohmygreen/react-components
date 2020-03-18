5ae2fffbd1f14c3c0b197d68fa6cc19a577e412e
Nemanja Lazarevic <nemanja.lazarevic.la@gmail.com>
2020-03-18 20:31:46 +0000

    add rollup external plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,29 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
 import replace from 'rollup-plugin-replace';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const outputFile = NODE_ENV === 'production' ? './dist/prod.js' : './dist/dev.js';
 
-export default {
-  input: './src/index.js',
-  output: {
-    file: outputFile,
-    format: 'cjs',
+export default [
+  {
+    input: './src/index.js',
+    output: {
+      file: outputFile,
+      format: 'cjs',
+    },
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+      }),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+    ],
   },
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-    }),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    resolve(),
-    commonjs(),
-  ],
-  external: id => /^react/.test(id),
-};
+];
